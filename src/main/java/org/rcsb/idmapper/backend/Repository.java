4b186936c708713cae70710fb35f468ac6a47023
Yana Rose <yana.valasatava@gmail.com>
2023-03-21 17:04:00 -0700

    remove static utils

--- src/main/java/org/rcsb/idmapper/backend/Repository.java
+++ src/main/java/org/rcsb/idmapper/backend/Repository.java
@@ -1,7 +1,6 @@
 package org.rcsb.idmapper.backend;
 
-import org.rcsb.idmapper.utils.AppUtils;
-
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -24,37 +23,59 @@ public class Repository {
     private final Map<String, String[]> polymerEntityToPolymerInstance = new ConcurrentHashMap<>();
     private final Map<String, String[]> polymerEntityToMolecularDefinition = new ConcurrentHashMap<>();
 
+    /**
+     * Concatenates two arrays
+     *
+     * @param array1 first array
+     * @param array2 second array
+     * @return resulting array contains data from {@param array1} and {@param array2}
+     */
+    private String[] concatenateArrays(String[] array1, String[] array2) {
+        String[] updated = new String[array1.length+array2.length];
+        System.arraycopy(array1, 0, updated, 0, array1.length);
+        System.arraycopy(array2, 0, updated, array1.length, array2.length);
+        return updated;
+    }
+
+    private void addNonEmptyValues(Map<String, String[]> map, String key, Collection<String> values) {
+        if (values == null || values.size() == 0)
+            return;
+        String[] existingValues = map.getOrDefault(key, new String[]{});
+        String[] updatedValues = concatenateArrays(existingValues, values.toArray(new String[0]));
+        map.put(key, updatedValues);
+    }
+
     public void addEntryToAssemblyMapping(String entryId, List<String> assemblyIds) {
-        AppUtils.addNonEmptyValues(entryToAssembly, entryId, assemblyIds);
+        addNonEmptyValues(entryToAssembly, entryId, assemblyIds);
     }
 
     public void addEntryToPubmedMapping(String entryId, Integer pubmedId) {
         if (pubmedId ==  null) return;
-        AppUtils.addNonEmptyValues(entryToPubmed, entryId, Collections.singleton(String.valueOf(pubmedId)));
+        addNonEmptyValues(entryToPubmed, entryId, Collections.singleton(String.valueOf(pubmedId)));
     }
 
     public void addEntryToPolymerEntity(String entryId, List<String> entityIds) {
-        AppUtils.addNonEmptyValues(entryToPolymerEntity, entryId, entityIds);
+        addNonEmptyValues(entryToPolymerEntity, entryId, entityIds);
     }
 
     public void addEntryToBranchedEntity(String entryId, List<String> entityIds) {
-        AppUtils.addNonEmptyValues(entryToBranchedEntity, entryId, entityIds);
+        addNonEmptyValues(entryToBranchedEntity, entryId, entityIds);
     }
 
     public void addEntryToNonPolymerEntity(String entryId, List<String> entityIds) {
-        AppUtils.addNonEmptyValues(entryToNonPolymerEntity, entryId, entityIds);
+        addNonEmptyValues(entryToNonPolymerEntity, entryId, entityIds);
     }
 
     public void addPolymerEntityToPolymerInstance(String entityId, List<String> instanceIds) {
-        AppUtils.addNonEmptyValues(polymerEntityToPolymerInstance, entityId, instanceIds);
+        addNonEmptyValues(polymerEntityToPolymerInstance, entityId, instanceIds);
     }
 
     public void addPolymerEntityToMonomers(String entityId, List<String> moleculeIds) {
-        AppUtils.addNonEmptyValues(polymerEntityToMolecularDefinition, entityId, moleculeIds);
+        addNonEmptyValues(polymerEntityToMolecularDefinition, entityId, moleculeIds);
     }
 
     public void addPolymerEntityToPrd(String entityId, String prdId) {
         if (prdId == null) return;
-        AppUtils.addNonEmptyValues(polymerEntityToMolecularDefinition, entityId, Collections.singleton(prdId));
+        addNonEmptyValues(polymerEntityToMolecularDefinition, entityId, Collections.singleton(prdId));
     }
 }
