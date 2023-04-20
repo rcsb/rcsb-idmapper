760eb7a67ecf9e447f95b85f2abfcf8c61ad5d03
Yana Rose <yana.valasatava@gmail.com>
2023-04-19 21:11:23 -0700

    organize input/output

--- src/main/java/org/rcsb/idmapper/frontend/AllInput.java
+++
@@ -1,5 +0,0 @@
-package org.rcsb.idmapper.frontend;
-
-public class AllInput extends Input {
-    public ContentType content_type;
-}
--- src/main/java/org/rcsb/idmapper/frontend/GroupInput.java
+++
@@ -1,7 +0,0 @@
-package org.rcsb.idmapper.frontend;
-
-public class GroupInput extends Input {
-    public String id;
-    public Input.AggregationMethod aggregation_method;
-    public int similarity_cutoff;
-}
