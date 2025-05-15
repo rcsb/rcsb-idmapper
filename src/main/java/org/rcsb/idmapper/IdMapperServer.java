0f66bae09be21dd64512bc7ed0f4f9866815ca85
josemduarte <jose.m.duarte@gmail.com>
2025-05-15 15:35:21 -0700

    Mongo user/pwd through specific env variables

--- src/main/java/org/rcsb/idmapper/IdMapperServer.java
+++ src/main/java/org/rcsb/idmapper/IdMapperServer.java
@@ -9,9 +9,12 @@ import org.rcsb.idmapper.frontend.UndertowFrontendImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Pattern;
 
 public class IdMapperServer {
 
@@ -21,11 +24,26 @@ public class IdMapperServer {
     public static final String GROUP = "/group";
     public static final String ALL = "/all";
     public static final String MONGODB_URI = "MONGODB_URI";
+    public static final String MONGODB_USER = "MONGODB_USER";
+    public static final String MONGODB_PWD = "MONGODB_PWD";
 
     public static void main(String[] args) {
 
-        var connectionString = Objects.requireNonNull(System.getenv(MONGODB_URI),
+        String connectionString = Objects.requireNonNull(System.getenv(MONGODB_URI),
                 String.format("The environment variable [ %s ] with Mongo database connection string (URI) must be set", MONGODB_URI));
+        int numPlaceHolders = (int) Pattern.compile("%s").matcher(connectionString).results().count();
+        if (numPlaceHolders != 2) {
+            LOGGER.error("Mongo connection URI string [ {} ] does not contain exactly 2 placeholders. " +
+                    "Please check '{}' env var", connectionString, MONGODB_URI);
+            throw new IllegalArgumentException("Mongo connection URI string does not contain exactly 2 placeholders");
+        }
+        String user = Objects.requireNonNull(System.getenv(MONGODB_USER), String.format("The environment variable [ %s ] with Mongo database user must be set", MONGODB_USER));
+        String pwd = Objects.requireNonNull(System.getenv(MONGODB_PWD), String.format("The environment variable [ %s ] with Mongo database password must be set", MONGODB_PWD));
+        // note anything that goes into the mongo URI must be URL-encoded
+        connectionString = String.format(connectionString,
+                URLEncoder.encode(user, StandardCharsets.UTF_8),
+                URLEncoder.encode(pwd, StandardCharsets.UTF_8));
+
         var backend = new BackendImpl(
                 new DataProvider(connectionString),
                 new Repository()
